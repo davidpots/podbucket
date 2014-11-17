a33d92f93a9490261b5433ad472f9ef9eb8e6c23
David Potsiadlo <davidpotsiadlo@learnzillion.com>
2014-11-17 09:22:05 -0500

    Logging in and out, through 8.3

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  root 'pages#home'
-  get  'signup'           => 'users#new'
+  root   'pages#home'
+  get    'signup'      => 'users#new'
+  get    'login'       => 'sessions#new'
+  post   'login'       => 'sessions#create'
+  delete 'logout'      => 'sessions#destroy'
   resources :users
 end
